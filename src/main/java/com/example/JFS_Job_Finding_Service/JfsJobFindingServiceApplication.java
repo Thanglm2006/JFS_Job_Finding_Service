c901961acedbee3f3a067a9cc0331de155c465ef
thanglm2006 <sungang.20072009@gmail.com>
2025-03-09 17:56:12 +0700

    JFS-init

---
+++ src/main/java/com/example/JFS_Job_Finding_Service/JfsJobFindingServiceApplication.java
@@ -0,0 +1,13 @@
+//package com.example.JFS_Job_Finding_Service;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class JfsJobFindingServiceApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JfsJobFindingServiceApplication.class, args);
+//    }
+//
+//}
