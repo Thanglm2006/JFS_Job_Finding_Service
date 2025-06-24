175f6d513d3f5b96f4d28c7a9623bc6659b188a8
thanglm2006 <sungang.20072009@gmail.com>
2025-06-25 01:42:18 +0700

    api

--- src/main/java/com/example/JFS_Job_Finding_Service/Controller/NotificationController.java
+++ src/main/java/com/example/JFS_Job_Finding_Service/Controller/NotificationController.java
@@ -3,9 +3,12 @@ package com.example.JFS_Job_Finding_Service.Controller;
 import com.example.JFS_Job_Finding_Service.Services.NotificationService;
 import com.example.JFS_Job_Finding_Service.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +19,7 @@ public class NotificationController {
     @Autowired
     private NotificationService notificationService;
     @GetMapping("/getAllNotifications")
-    public Object getAllNotifications(String token) {
-        return notificationService.getAllNotifications(token);
+    public Object getAllNotifications(@RequestHeader HttpHeaders headers) {
+        return notificationService.getAllNotifications(headers.getFirst("token"));
     }
 }
