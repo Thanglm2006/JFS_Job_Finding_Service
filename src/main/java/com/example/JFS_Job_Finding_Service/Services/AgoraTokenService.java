775216ccc731092ff079c0330833b10a33e4e610
thanglm2006 <sungang.20072009@gmail.com>
2025-12-05 17:18:58 +0700

    update docker

--- src/main/java/com/example/JFS_Job_Finding_Service/Services/AgoraTokenService.java
+++ src/main/java/com/example/JFS_Job_Finding_Service/Services/AgoraTokenService.java
@@ -1,35 +1,42 @@
 package com.example.JFS_Job_Finding_Service.Services;
+
 import io.agora.media.RtcTokenBuilder2;
 import io.agora.media.RtcTokenBuilder2.Role;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class AgoraTokenService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AgoraTokenService.class);
+
     @Value("${spring.agora.id}")
     private String APP_ID;
+
     @Value("${spring.agora.cer}")
-    private  String APP_CERTIFICATE;
-    private static final int EXPIRATION_TIME = 3600; // 1 hour
+    private String APP_CERTIFICATE;
+
+    private static final int EXPIRATION_TIME_IN_SECONDS = 3600; // 1 hour
 
     public String generateToken(String channelName, int uid) {
         RtcTokenBuilder2 tokenBuilder = new RtcTokenBuilder2();
-        long timestamp = (System.currentTimeMillis() / 1000) + EXPIRATION_TIME;
 
-        // Role.ROLE_PUBLISHER = User can talk/stream video
-        // Role.ROLE_SUBSCRIBER = User can only watch
-        String token=tokenBuilder.buildTokenWithUid(
+        int timestamp = (int) (System.currentTimeMillis() / 1000 + EXPIRATION_TIME_IN_SECONDS);
+
+        String token = tokenBuilder.buildTokenWithUid(
                 APP_ID,
                 APP_CERTIFICATE,
                 channelName,
                 uid,
                 Role.ROLE_PUBLISHER,
-                (int) timestamp,
-                (int) timestamp
+                timestamp, // privilegeExpiration:
+                timestamp  // tokenExpiration:
         );
-        System.out.println(APP_ID);
-        System.out.println(APP_CERTIFICATE);
-        System.out.println("token:"+token);
+
+        logger.info("Generated Agora Token for Channel: {}, UID: {}", channelName, uid);
+
         return token;
     }
 }
