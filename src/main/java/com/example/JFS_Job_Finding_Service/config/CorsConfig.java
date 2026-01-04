b768f267cead0a10ebd3debeb5028663f0938f27
thanglm2006 <sungang.20072009@gmail.com>
2026-01-04 20:39:52 +0700

    ok

--- src/main/java/com/example/JFS_Job_Finding_Service/config/CorsConfig.java
+++ src/main/java/com/example/JFS_Job_Finding_Service/config/CorsConfig.java
@@ -15,16 +15,19 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("https://test.job-fs.me"
-//                , "https://www.test.job-fs.me"
-//                , "http://localhost:5173"
-//                ,"https://danhdeptrai.vercel.app"
-//                ,"https://www.danhdeptrai.vercel.app"
-//                , "https://www.job-fs.me"
-//                , "https://job-fs.me"
-//                , "https://www.admin.job-fs.me"
-//                , "https://admin.job-fs.me"));
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("https://test.job-fs.me"
+                , "https://www.test.job-fs.me"
+                , "http://localhost:5173"
+                ,"http://localhost:5870"
+                ,"http://localhost:5871"
+                ,"http://localhost:5872"
+                ,"https://danhdeptrai.vercel.app"
+                ,"https://www.danhdeptrai.vercel.app"
+                , "https://www.job-fs.me"
+                , "https://job-fs.me"
+                , "https://www.admin.job-fs.me"
+                , "https://admin.job-fs.me"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
